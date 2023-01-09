d76248b7d456057d16aa68397492f8c8ed6f63bc
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-09 17:36:30 -0300

    About view and controller, util classes and icon added

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -1,11 +1,22 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import main.Main;
+import util.AlertDialog;
 
 /**
  * FXML Controller class
@@ -13,9 +24,13 @@ import javafx.scene.control.MenuItem;
  * @author joana
  */
 public class MainViewController implements Initializable {
-    @FXML private MenuItem menuItemSeller;
-    @FXML private MenuItem menuItemDepartment;
-    @FXML private MenuItem menuItemAbout;
+
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
 
     /**
      * Initializes the controller class.
@@ -23,7 +38,7 @@ public class MainViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void handleMenuItemSeller(ActionEvent event) {
@@ -35,5 +50,27 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void handleMenuItemAbout(ActionEvent event) {
-    }   
+        loadView("../view/AboutView.fxml", x -> {
+        });
+    }
+
+    private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
+
+            T controller = loader.getController();
+            initializingAction.accept(controller);
+        } catch (IOException e) {
+            AlertDialog.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+        }
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -12,6 +13,8 @@ import javafx.stage.Stage;
  * @author joana
  */
 public class Main extends Application {
+    
+    private static Scene mainScene;
 
     @Override
     public void start(Stage primaryStage) {
@@ -22,13 +25,18 @@ public class Main extends Application {
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("JavaFX application");
+            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../icon/icon.png")));
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     /**
